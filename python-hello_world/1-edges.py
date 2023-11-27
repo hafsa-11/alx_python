f73bed532dbff36d50d83e67fcb68169cda2a79e
hafsa-11 <hafsa98lakhouaja@gmail.com>
2023-11-27 09:37:31 +0100

    upload

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,3 +1,3 @@
-print("First 3 letters: {}".format(Hol))
-print("Last 2 letters: {}".format(on))
-print("Middle word: {}".format(olberto))
+print("First 3 letters: Hol ")
+print("Last 2 letters: on ")
+print("Middle word: olberto ")
