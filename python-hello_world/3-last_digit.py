4fe49dd4a1450e60934618e40bcda7c7ad3a20ec
hafsa-11 <hafsa98lakhouaja@gmail.com>
2023-11-27 11:20:54 +0100

    upload

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -1,14 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 import random
 
 number = random.randint(-10000, 10000)
 last_digit = abs(number) % 10
 
-print("Last digit of", number, "is", -last_digit, end=" ")
+print("Last digit of {} is {} and is ".format(number, -last_digit if number < 0 else last_digit), end="")
 
-if last_digit > 5:
-    print("and is greater than 5")
-elif last_digit == 0:
-    print("and is 0")
+if last_digit == 0 and number != 0:
+    print("0")
+elif number < 0:
+    print("less than 6 and not 0")
 else:
-    print("and is less than 6 and not 0")
+    print("greater than 5" if number != 0 else "0")
