e0f324e3e67c9459676c47d561460502f005c03c
hafsa-11 <hafsa98lakhouaja@gmail.com>
2023-11-27 10:31:34 +0100

    upload

---
+++ python-hello_world/2-positive_or_negative.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+import random
+
+number = random.randint(-10, 10)
+print("{} is ".format(number), end="")
+
+if number > 0:
+    print("positive")
+elif number == 0:
+    print("zero")
+else:
+    print("negative")
