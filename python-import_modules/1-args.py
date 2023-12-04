08573f2479f68f20dd68caedc2cf8f47ffe9b69c
hafsa-11 <hafsa98lakhouaja@gmail.com>
2023-12-04 13:54:55 +0100

    upload

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,11 +1,11 @@
+#!/usr/bin/python3
 from sys import argv
 
 if _name_ == "_main_":
     num_args = len(argv) - 1
-    plural_s = "s" if num_args != 1 else ""
-    
-    print("Number of argument{}: {}{}".format(plural_s, num_args, ":" if num_args else "."))
-    
-    if num_args > 0:
-        for i, arg in enumerate(argv[1:], start=1):
-            print("{}: {}".format(i, arg))
+    plural_suffix = 's' if num_args != 1 else ''
+
+    print("{} argument{}".format(num_args, plural_suffix) + (':' if num_args else '.'))
+
+    for i, arg in enumerate(argv[1:], start=1):
+        print("{}: {}".format(i, arg))
