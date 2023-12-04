43180c8a0a64d19828415115abb15844bb572f92
hafsa-11 <hafsa98lakhouaja@gmail.com>
2023-12-04 14:03:22 +0100

    upload

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-from add_0 import add
-
 a = 1
 b = 2
-print("{} + {} = {}".format(a, b, add(a, b)))
+
 if _name_ == "_main_":
-    pass
+    add_0 = _import_('add_0')
+    print("{} + {} = {}".format(a, b, add_0.add(a, b)))
