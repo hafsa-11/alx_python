9c1095f4453bbf342ec30a788896e0d446f23246
hafsa-11 <hafsa98lakhouaja@gmail.com>
2023-12-11 10:23:16 +0100

    upload

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,3 +1,9 @@
+"""
+Module: 2-square
+
+This module defines the Square class, representing a square by its size.
+"""
+
 class Square:
     """
     This is the Square class that defines a square by its size.
@@ -20,9 +26,4 @@ class Square:
 
     def area(self):
         """
-        Computes and returns the area of the square.
-
-        Returns:
-        - int: The area of the square.
-        """
-        return self.__size ** 2
+        Computes and returns⬤
