0e61ca19c40ac28d79796a28a17bcee2bbfc5c67
hafsa-11 <hafsa98lakhouaja@gmail.com>
2023-12-11 12:53:32 +0100

    upload

---
+++ python-classes/4-square.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+"""Defines a Square class."""
+
+class Square:
+    """Represents a square."""
+    
+    def __init__(self, size=0):
+        """Initializes a new Square.
+
+        Args:
+            size (int): The size of the square.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """Gets the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets the size of the square.
+
+        Args:
+            value (int): The new size of the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculates the area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        """Prints the square using '#' characters."""
+        if self.__size == 0:
+            print()
+        else:
+            for _ in range(self.__size):
+                print("#" * self.__size)
+
+if __name__ == "__main__":
+    my_square = Square(3)
+    my_square.my_print()
+
+    print("--")
+
+    my_square.size = 10
+    my_square.my_print()
+
+    print("--")
+
+    my_square.size = 0
+    my_square.my_print()
+
+    print("--")
