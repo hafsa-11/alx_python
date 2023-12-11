df14838b614dffd3568fe80646c16058bb665ed1
hafsa-11 <hafsa98lakhouaja@gmail.com>
2023-12-11 10:09:19 +0100

    upload

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,12 @@
+"""
+Module: square
+
+This module defines the Square class, representing a square by its size.
+"""
+
 class Square:
     """
-    This is a Square class that defines a square by its size.
+    This is the Square class that defines a square by its size.
     """
 
     def _init_(self, size):
@@ -11,27 +17,3 @@ class Square:
         - size (int): The size of the square.
         """
         self.__size = size
-
-# Corrected output cases:
-
-# Case 1:
-mysquare = Square(3)
-print(type(mysquare))  # Output: <class '_main_.Square'>
-print(mysquare._dict)  # Output: {'_Square_size': 3}
-
-# Case 2:
-mysquare = Square(89)
-print(type(mysquare))  # Output: <class '_main_.Square'>
-print(mysquare._dict)  # Output: {'_Square_size': 89}
-
-# Case 3:
-try:
-    print(mysquare.size)
-except Exception as e:
-    print(e)  # Output: 'Square' object has no attribute 'size'
-
-# Case 4:
-try:
-    print(mysquare._size)
-except Exception as e:
-    print(e)  # Output: 89
