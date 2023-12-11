3d885863d69e309690844c985ad37c89d6268ed5
hafsa-11 <hafsa98lakhouaja@gmail.com>
2023-12-11 14:41:24 +0100

    upload

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python3
+"""
+Module: 0-is_same_class
+
+This module defines the is_same_class function.
+"""
+
 def is_same_class(obj, a_class):
     """
     Check if obj is exactly an instance of a_class.
@@ -9,4 +16,13 @@ def is_same_class(obj, a_class):
     Returns:
     - bool: True if obj is exactly an instance of a_class, otherwise False.
     """
+
     return type(obj) is a_class
+
+a = 1
+if is_same_class(a, int):
+    print("{} is an instance of the class {}".format(a, int._name_))
+if is_same_class(a, float):
+    print("{} is an instance of the class {}".format(a, float._name_))
+if is_same_class(a, object):
+    print("{} is an instance of the class {}".format(a, object._name_))
