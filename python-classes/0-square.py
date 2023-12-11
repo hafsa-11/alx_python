8fe0533e7968996a46037b832e3f3c1a36020ab2
hafsa-11 <hafsa98lakhouaja@gmail.com>
2023-12-11 10:11:19 +0100

    upload

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,19 +1,39 @@
+#!/usr/bin/python3
 """
-Module: square
+This module defines a Square class.
 
-This module defines the Square class, representing a square by its size.
+The Square class has a private instance attribute 'size'.
 """
 
 class Square:
     """
-    This is the Square class that defines a square by its size.
+    This class represents a square.
+
+    Attributes:
+        __size (int): The size of the square.
     """
 
     def _init_(self, size):
         """
         Initializes a new Square instance.
 
-        Parameters:
-        - size (int): The size of the square.
+        Args:
+            size (int): The size of the square.
         """
         self.__size = size
+
+if _name_ == "_main_":
+    # Example of using the Square class
+    my_square = Square(3)
+    print(type(my_square))
+    print(my_square._dict_)
+
+    try:
+        print(my_square.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square.__size)
+    except Exception as e:
+        print(e)
