499141c1047fdb9ee6a1d9242b00a3c9e7392f2d
hafsa-11 <hafsa98lakhouaja@gmail.com>
2023-12-11 10:56:58 +0100

    upload

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,47 +1,40 @@
 """
-Module: 2-square
-
-This module defines the Square class, representing a square by its size.
+2-square.py - a module that defines the Square class
 """
 
 class Square:
     """
-    This is the Square class that defines a square by its size.
-    """
+    This class represents a square.
 
-    def _init_(self, size=0):
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size=0): Initializes a new Square instance.
+        area(self): Calculates and returns the area of the square.
+    """
+    def __init__(self, size=0):
         """
         Initializes a new Square instance.
 
-        Parameters:
-        - size (int, optional): The size of the square. Defaults to 0.
+        Args:
+            size (int): The size of the square. Default is 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
-
         self.__size = size
 
     def area(self):
         """
-        Computes and returns the area of the square.
+        Calculates and returns the area of the square.
 
         Returns:
-        - int: The area of the square.
+            int: The area of the square.
         """
         return self.__size ** 2
-
-
-# Test case 1
-mysquare = Square(3)
-print("{}".format(mysquare.area()))  # Output: 9
-
-# Test case 2
-mysquare = Square(89)
-print("{}".format(mysquare.area()))  # Output: 7921
-
-# Test case 3
-mysquare = Square()
-print("{}".format(mysquare.area()))  # Output: 0
