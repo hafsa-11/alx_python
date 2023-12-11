46120fa7592cc5d2d6e0aba724bc01739740c0ae
hafsa-11 <hafsa98lakhouaja@gmail.com>
2023-12-11 10:45:21 +0100

    upload

---
+++ python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module documentation"""
+class Square:
+    """Square class"""
+
+    def _init_(self, size=0):
+        """Initialize the Square instance"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
