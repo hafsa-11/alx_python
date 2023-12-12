71a166126caf12d6d637cb768f9e19c508b443aa
hafsa-11 <hafsa98lakhouaja@gmail.com>
2023-12-12 09:41:38 +0100

    upload

--- python-inheritance/1-is_kind_of_class.py
+++ python-inheritance/1-is_kind_of_class.py
@@ -1,4 +1,12 @@
 def is_kind_of_class(obj, a_class):
+    """
+    Check if the object is an instance of, or if it is an instance
+    of a class that inherited from the specified class.
+
+    :param obj: The object to check.
+    :param a_class: The class or a tuple of classes to check against.
+    :return: True if the object is an instance of the specified class, else False.
+    """
     return isinstance(obj, a_class)
 
 # Test cases
