4cd628eef2846788d1c92d46ceb4ae051a68cdf2
hafsa-11 <hafsa98lakhouaja@gmail.com>
2023-12-12 14:55:40 +0100

    upload

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -3,10 +3,12 @@ This module provides a base geometry class.
 """
 
 
-class BaseGeometry:BaseGeometry = __import__('3-base_geometry').BaseGeometry
-
+class BaseGeometry=
+    pass
 
 bg = BaseGeometry()
+BaseGeometry = __import__('3-base_geometry').BaseGeometry
+
 
 print(bg)
 print(dir(bg))
