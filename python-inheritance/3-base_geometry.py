e655d82549e3982bb14bea6dcdcbb988d7f15871
hafsa-11 <hafsa98lakhouaja@gmail.com>
2023-12-12 14:30:41 +0100

    upload

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -6,13 +6,4 @@ class BaseGeometry:
     """
     An empty class representing base geometry.
     """
-
-    def __repr__(self):
-        """
-        Override the __repr__ method to customize the representation.
-        """
-        return "<{} object at {}>".format(self.__class__.__name__, hex(id(self)))
-
-# Example usage
-bg = BaseGeometry()
-print(bg)
+    pass
