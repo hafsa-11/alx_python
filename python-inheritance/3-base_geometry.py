d5604be8514dd9f41168cd27d96882317b353dcb
hafsa-11 <hafsa98lakhouaja@gmail.com>
2023-12-12 14:45:57 +0100

    upload

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -2,17 +2,13 @@
 This module provides a base geometry class.
 """
 
+#!/usr/bin/python3
+
 class BaseGeometry:
-    """
-    An empty class representing base geometry.
-    """
     pass
 
-    def __dir__(self):
-        """
-        Override the __dir__ method to exclude the default attributes and methods.
-        """
-        return ['__doc__', '__module__']
+bg = BaseGeometry()
 
-# Example usage
+print(bg)
+print(dir(bg))
 print(dir(BaseGeometry))
