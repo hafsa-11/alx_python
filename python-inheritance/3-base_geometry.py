3c3d6148686a35d17c04698ca0450759b6fd81d4
hafsa-11 <hafsa98lakhouaja@gmail.com>
2023-12-12 14:32:07 +0100

    upload

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -6,4 +6,12 @@ class BaseGeometry:
     """
     An empty class representing base geometry.
     """
-    pass
+
+    def __dir__(self):
+        """
+        Override the __dir__ method to exclude the default attributes and methods.
+        """
+        return ['__doc__', '__module__']
+
+# Example usage
+print(dir(BaseGeometry))
