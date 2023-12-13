a50dd033845303e49cc76d39636c84567ebfd9fa
hafsa-11 <hafsa98lakhouaja@gmail.com>
2023-12-13 12:23:36 +0100

    upload

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -9,15 +9,14 @@ class BaseGeometry:
     This class serves as the base class for geometry-related classes.
     It is currently empty and intended for further development.
     """
-
-    def __dir__(self):
-        """Custom implementation for dir().
-
-        Returns:
-            list: List of attributes for dir() to display.
-        """
-        return ['__class__', '__dict__', '__dir__', '__doc__', '__module__']
-
+    
 bg = BaseGeometry()
+
+# Correct output - case: bg = BaseGeometry() print(bg)
+print(bg)
+
+# Correct output - case: bg = BaseGeometry() print(dir(bg))
 print(dir(bg))
+
+# Correct output - case: print(dir(BaseGeometry))
 print(dir(BaseGeometry))
