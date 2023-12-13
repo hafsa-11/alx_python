76537db45a3359aa9d564beeacbb2d47014ada45
hafsa-11 <hafsa98lakhouaja@gmail.com>
2023-12-13 12:31:15 +0100

    upload

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,15 +1,24 @@
+"""4-base_geometry module
+
+This module defines the BaseGeometry class.
+"""
+
 class BaseGeometry:
-    """
-    A base geometry class.
+    """BaseGeometry class
+
+    This class serves as the base class for geometry-related classes.
     """
 
     def area(self):
-        """
-        Public instance method that raises an Exception with the message "area() is not implemented".
+        """Public instance method that raises an Exception.
+
+        Raises:
+            Exception: Always raises an Exception with the message
+                "area() is not implemented".
         """
         raise Exception("area() is not implemented")
 
-try:
+if __name__ == "__main__":
     BaseGeometry = __import__('4-base_geometry').BaseGeometry
 
     bg = BaseGeometry()
@@ -18,5 +27,3 @@ try:
         print(bg.area())
     except Exception as e:
         print("[{}] {}".format(e.__class__.__name__, e))
-except ImportError:
-    print("Error importing BaseGeometry from '4-base_geometry'")
