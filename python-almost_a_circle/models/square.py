28b23954ff772f9856f2c395ab143acf82bacffe
hafsa-11 <hafsa98lakhouaja@gmail.com>
2023-12-18 10:33:32 +0100

    upload

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -17,6 +17,17 @@ class Square(Rectangle):
         """
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Getter for size."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Setter for size."""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Return a string representation of the square."""
         return "[Square] ({}) {}/{} - {}".format(
