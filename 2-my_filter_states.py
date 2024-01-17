5c5f7004e133770c88fc111032860e3f1ba59125
hafsa-11 <hafsa98lakhouaja@gmail.com>
2024-01-17 10:18:45 +0100

    up

---
+++ 2-my_filter_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 5:
+        print("Usage: {} <username> <password> <database> <state_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Get MySQL username, password, database, and state name from command line arguments
+    username, password, database, state_name = sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4]
+
+    # Connect to MySQL server
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    # Create a cursor object to execute SQL queries
+    cursor = db.cursor()
+
+    # Use format to create the SQL query with the user input
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY id".format(state_name)
+
+    # Execute the SELECT query to fetch states with the specified name
+    cursor.execute(query)
+
+    # Fetch all the rows and print them
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    # Close the cursor and database connection
+    cursor.close()
+    db.close()
