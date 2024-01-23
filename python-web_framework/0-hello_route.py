301778c4cb26f72fea06cf53d263e40e456d4b27
hafsa-11 <hafsa98lakhouaja@gmail.com>
2024-01-23 09:37:47 +0100

    up

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -2,11 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-# Define the route with strict_slashes=False
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
     return 'Hello HBNB!'
 
 if __name__ == '__main__':
-    # Run the Flask application on 0.0.0.0, port 5000
     app.run(host='0.0.0.0', port=5000)
