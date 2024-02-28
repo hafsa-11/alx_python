6e24182271dffcb731772d546c843cfca5c3f32a
hafsa-11 <hafsa98lakhouaja@gmail.com>
2024-02-28 11:02:43 +0100

    up

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,15 +1,45 @@
+"""
+Export User Tasks to JSON
+
+This script fetches tasks owned by a specific user from an API and exports them to a JSON file.
+The exported JSON file contains task details such as task title, completion status, and username.
+
+Usage:
+    python3 script_name.py <USER_ID>
+
+Example:
+    python3 script_name.py 2
+"""
+
 import json
 import requests
 import sys
 
 def fetch_user_tasks(user_id):
-    # Fetching tasks for the specified user
+    """
+    Fetch tasks for the specified user from an API.
+
+    Args:
+        user_id (int): The ID of the user whose tasks are to be fetched.
+
+    Returns:
+        list: A list of tasks owned by the specified user.
+    """
     response = requests.get(f'https://jsonplaceholder.typicode.com/todos?userId={user_id}')
     tasks = response.json()
     return tasks
 
 def export_to_json(user_id, tasks):
-    # Creating JSON object
+    """
+    Export user tasks to a JSON file.
+
+    Args:
+        user_id (int): The ID of the user whose tasks are being exported.
+        tasks (list): A list of tasks owned by the user.
+
+    Returns:
+        None
+    """
     data = {str(user_id): []}
     for task in tasks:
         task_data = {
@@ -19,12 +49,14 @@ def export_to_json(user_id, tasks):
         }
         data[str(user_id)].append(task_data)
 
-    # Writing JSON object to a file
     filename = f'{user_id}.json'
     with open(filename, 'w') as f:
         json.dump(data, f, indent=2)
 
 def main():
+    """
+    Main function to fetch user tasks and export them to a JSON file.
+    """
     if len(sys.argv) != 2:
         print("Usage: python3 script_name.py <USER_ID>")
         sys.exit(1)
@@ -42,3 +74,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
